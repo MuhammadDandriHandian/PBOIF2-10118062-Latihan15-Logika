abb9908bbe681addde830e652f05d6ede960430d
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-09-26 14:46:22 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan15-Logika/src/pboif2/pkg10118062/latihan15/logika/PBOIF210118062Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * Nama     : Muhammad Dandri Handian
+ * Kelas    : IF-2
+ * NIM      : 10118062
+ */
+public class PBOIF210118062Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a && b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b = " + !(a && b));
+    }
+    
+}
